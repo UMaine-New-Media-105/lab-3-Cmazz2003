a2db37d5f89794a389898add2a30ab29119be84c
Cmazz2003 <124199380+Cmazz2003@users.noreply.github.com>
2023-02-15 14:13:35 -0500

    Abstract shape

---
+++ Ex1.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+fill("gold")
+scale(2)
+translate(40,5)
+triangle(30, 75, 58, 15, 86, 75);
+triangle(5, 140, 30, 75, 60, 140);
+triangle(60, 140, 86, 75, 115, 140)
+if (mouseX > 200)
+stroke("gold")
+else if (mouseX < 200)
+stroke(50);
+
+
+}
